4f8b621f3661a73ce202ac582ba5d5093fec1cfe
Aaron Schlesinger <aaron@deis.com>
2016-11-04 10:45:36 -0700

    feat(k8s/data): add new data types based on SIG
    In the 11/2-11/3/2016 sig-service-catalog discussions in Boulder at the
    Deis offices, the group came to consensus on what Kubernetes resources
    to use (the nouns) and how they’ll be used (the API). Both concepts are
    captured briefly in
    https://github.com/kubernetes-incubator/service-catalog/pull/31.
    
    This PR adds the nouns to the steward framework codebase. Future
    patches will begin incorporating them into the state machine to address
    the API changes

---
+++ k8s/data/broker.go
@@ -0,0 +1,32 @@
+package data
+
+import (
+	"k8s.io/client-go/1.4/pkg/api/unversioned"
+	"k8s.io/client-go/1.4/pkg/api/v1"
+)
+
+type BrokerState string
+
+const (
+	BrokerStatePending   BrokerState = "Pending"
+	BrokerStateAvailable BrokerState = "Available"
+	BrokerStateFailed    BrokerState = "Failed"
+)
+
+type Broker struct {
+	unversioned.TypeMeta `json:",inline"`
+	v1.ObjectMeta        `json:"metadata,omitempty"`
+
+	Spec   BrokerSpec
+	Status BrokerStatus
+}
+
+type BrokerSpec struct {
+	URL      string `json:"url"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type BrokerStatus struct {
+	State BrokerState `json:"state"`
+}
