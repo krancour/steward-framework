012822cf1f5e5c64f44c01b42cf7cfdd123bb207
Aaron Schlesinger <aaron@deis.com>
2016-11-09 11:19:22 -0800

    fix(k8s/broker): add create lifecycler func

--- k8s/broker/loop.go
+++ k8s/broker/loop.go
@@ -4,13 +4,16 @@ import (
 	"context"
 
 	"github.com/deis/steward-framework"
+	"github.com/deis/steward-framework/k8s/data"
 	"k8s.io/client-go/pkg/watch"
 )
 
 // WatchBrokerFunc is the function that returns a watch interface for broker resources
 type WatchBrokerFunc func() (watch.Interface, error)
 
+type CreateLifecyclerFunc func(data.Broker) (framework.Lifecycler, error)
+
 // RunLoop starts a blocking control loop that watches and takes action on broker resources
-func RunLoop(ctx context.Context, fn WatchBrokerFunc, lifecycler framework.Lifecycler) error {
+func RunLoop(ctx context.Context, fn WatchBrokerFunc, lifecyclerFn CreateLifecyclerFunc) error {
 	return nil
 }
