fabae43fece97cecb3ad20599391e2205805e12c
Aaron Schlesinger <aaron@deis.com>
2016-11-09 13:48:49 -0800

    fix(k8s/broker): create a broker add handler

--- k8s/broker/loop.go
+++ k8s/broker/loop.go
@@ -2,18 +2,57 @@ package broker
 
 import (
 	"context"
+	"errors"
 
 	"github.com/deis/steward-framework"
 	"github.com/deis/steward-framework/k8s/data"
 	"k8s.io/client-go/pkg/watch"
 )
 
+var (
+	ErrCancelled  = errors.New("stopped")
+	ErrNotABroker = errors.New("not a broker")
+)
+
 // WatchBrokerFunc is the function that returns a watch interface for broker resources
 type WatchBrokerFunc func() (watch.Interface, error)
 
-type CreateLifecyclerFunc func(data.Broker) (framework.Lifecycler, error)
-
 // RunLoop starts a blocking control loop that watches and takes action on broker resources
-func RunLoop(ctx context.Context, fn WatchBrokerFunc, lifecyclerFn CreateLifecyclerFunc) error {
+func RunLoop(ctx context.Context, fn WatchBrokerFunc, cataloger framework.Cataloger) error {
+	watcher, err := fn()
+	if err != nil {
+		return err
+	}
+	ch := watcher.ResultChan()
+	defer watcher.Stop()
+	for {
+		select {
+		case <-ctx.Done():
+			return ErrCancelled
+		case evt := <-ch:
+			switch evt.Type {
+			case watch.Added:
+				if err := handleAddBroker(cataloger, evt); err != nil {
+					return err
+				}
+			}
+			if err := handleEvt(evt); err != nil {
+				return err
+			}
+		}
+	}
+}
+
+func handleAddBroker(ctx, cataloger framework.Cataloger, evt watch.Event) error {
+	broker, ok := evt.Object.(data.Broker)
+	if !ok {
+		return ErrNotABroker
+	}
+	svcs, err := cataloger.List(ctx, broker.Spec)
+	if err != nil {
+		return err
+	}
+	// TODO: write services to ServiceClasses
 	return nil
+
 }
