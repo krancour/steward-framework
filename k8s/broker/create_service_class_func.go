b4262611afa87018dee59bccaaf727697d84abff
Aaron Schlesinger <aaron@deis.com>
2016-11-11 09:22:04 -0800

    fix(k8s/broker): move fake broker funcs into test files

--- k8s/broker/create_service_class_func.go
+++ k8s/broker/create_service_class_func.go
@@ -22,17 +22,3 @@ func NewK8sCreateServiceClassFunc(restIface rest.Interface) CreateServiceClassFu
 		return restIface.Post().AbsPath(url...).Do().Error()
 	}
 }
-
-// returns the function and a mutable slice of classes that were created. if retErr != nil,
-// it is always returned by the function and the returned slice is never modified
-func newFakeCreateServiceClassFunc(retErr error) (CreateServiceClassFunc, *[]*data.ServiceClass) {
-	createdClasses := []*data.ServiceClass{}
-	retFn := func(sClass *data.ServiceClass) error {
-		if retErr != nil {
-			return retErr
-		}
-		createdClasses = append(createdClasses, sClass)
-		return nil
-	}
-	return retFn, &createdClasses
-}
